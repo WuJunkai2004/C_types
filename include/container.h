c12232ab2e2754cfdf6c94220c385c49d136e46a
Wu Junkai <wu.junkai@qq.com>
2024-08-29 18:15:10 +0800

    feat: 完善了队列的逻辑

--- include/container.h
+++ include/container.h
@@ -35,11 +35,27 @@ int   stack_is_empty(stack*);
  * @param _max_size: max size of queue, you can't access the data directly
  */
 typedef struct{
-    list _inner_list;
+    void *_data;
     int _front;
     int _rear;
+    int _data_size;
     int _max_size;
-    int _is_smart_resize;
 } queue;
+
+queue queue_create_by(int, int);
+void  queue_push_ptr(queue*, void*);
+void* queue_front_ptr(queue*);
+void* queue_back_ptr(queue*);
+void* queue_pop_front_ptr(queue*);
+void* queue_pop_back_ptr(queue*);
+int   queue_size(queue*);
+void  queue_clear(queue*);
+void  queue_free(queue*);
+#define queue_create(type, size) ({queue_create_by(sizeof(type), size);})
+#define queue_push(q, value) ({typeof(value) _v=(value); queue_push_ptr((q), &_v);})
+#define queue_front(type, q) ({*(type*)queue_front_ptr((q));})
+#define queue_back(type, q) ({*(type*)queue_back_ptr((q));})
+#define queue_pop_front(type, q) ({*(type*)queue_pop_front((q));})
+#define queue_pop_back(type, q) ({*(type*)queue_pop_back((q));})
 
 #endif
--- src/container.c
+++ src/container.c
@@ -92,7 +92,118 @@ int stack_is_empty(stack* this) {
  * @param s 栈
  */
 void stack_clear(stack* this) {
-    while(!stack_is_empty(this)){
-        stack_pop_ptr(this);
+    this->_idx = -1;
+}
+
+
+/**
+ * @brief 创建一个队列
+ * @param sizeof(type) 类型的大小
+ * @param size 队列的大小
+ */
+queue queue_create_by(int sizeof_type, int size) {
+    queue q;
+    q._data = malloc(sizeof_type * (size + 1));
+    q._data_size = sizeof_type;
+    q._front = 0;
+    q._rear = 0;
+    q._max_size = size + 1;
+    return q;
+}
+
+
+/**
+ * @brief 入队
+ * @param q 队列
+ * @param value 值
+ */
+void queue_push_ptr(queue* this, void* value) {
+    if(queue_size(this) >= this->_max_size){
+        return;
     }
+    memcpy(this->_data + this->_rear * this->_data_size, value, this->_data_size);
+    this->_rear = (this->_rear + 1) % this->_max_size;
+}
+
+
+/**
+ * @brief 获取队首元素
+ * @param q 队列
+ * @return void* 返回队首元素
+ */
+void* queue_front_ptr(queue* this) {
+    if(queue_size(this) <= 0){
+        return NULL;
+    }
+    return this->_data + this->_front * this->_data_size;
+}
+
+
+/**
+ * @brief 获取队尾元素
+ * @param q 队列
+ * @return void* 返回队尾元素
+ */
+void* queue_back_ptr(queue* this) {
+    if(queue_size(this) <= 0){
+        return NULL;
+    }
+    return this->_data + this->_rear * this->_data_size;
+}
+
+
+/**
+ * @brief 队首元素出队
+ * @param q 队列
+ * @return void* 返回出队的元素
+ */
+void* queue_pop_front_ptr(queue* this) {
+    if(queue_size(this) <= 0){
+        return NULL;
+    }
+    void* ret = this->_data + this->_front * this->_data_size;
+    this->_front = (this->_front + 1) % this->_max_size;
+    return ret;
+}
+
+
+/**
+ * @brief 队尾元素出队
+ * @param q 队列
+ * @return void* 返回出队的元素
+ */
+void* queue_pop_back_ptr(queue* this) {
+    if(queue_size(this) <= 0){
+        return NULL;
+    }
+    this->_rear = (this->_rear - 1 + this->_max_size) % this->_max_size;
+    return this->_data + this->_rear * this->_data_size;
+}
+
+
+/**
+ * @brief 获取队列的大小
+ * @param q 队列
+ * @return int 返回队列的大小
+ */
+int queue_size(queue* this) {
+    return (this->_rear - this->_front + this->_max_size) % this->_max_size;
+}
+
+
+/**
+ * @brief 清空队列
+ * @param q 队列
+ */
+void queue_clear(queue* this) {
+    this->_front = this->_rear = 0;
+}
+
+
+/**
+ * @brief 释放队列
+ * @param q 队列
+ */
+void queue_free(queue* this) {
+    free(this->_data);
 }
