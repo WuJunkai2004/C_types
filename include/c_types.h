c401bdec3ad1962fd70829fbd35d9430770804d2
Wu Junkai <wu.junkai@qq.com>
2025-09-14 10:12:09 +0800

    fix: 移除不必要的类型定义

--- include/c_types.h
+++ include/c_types.h
@@ -1,18 +1,6 @@
 #ifndef __C_TYPES_HEADER__
 #define __C_TYPES_HEADER__
 
-typedef char int8;
-typedef unsigned char uint8;
-
-typedef short int int16;
-typedef unsigned short int uint16;
-
-typedef int int32;
-typedef unsigned int uint32;
-
-typedef long long int64;
-typedef unsigned long long uint64;
-
 typedef int bool;
 #define true 1
 #define false 0
