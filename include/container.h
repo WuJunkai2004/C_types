5742f371bbd3e65f7928cf36b796d4e65e74ec76
Wu Junkai <wu.junkai@qq.com>
2025-09-14 10:26:33 +0800

    fix: 修正栈和队列的类型定义为 stack_t 和 queue_t

--- include/container.h
+++ include/container.h
@@ -11,16 +11,16 @@
 typedef struct{
     list _inner_list;
     int _idx;
-} stack;
+} stack_t;
 
-stack stack_create_by(int);
-void  stack_push_ptr(stack*, void*);
-void* stack_top_ptr(stack*);
-void* stack_pop_ptr(stack*);
-int   stack_size(stack*);
-void  stack_clear(stack*);
-void  stack_free(stack*);
-int   stack_is_empty(stack*);
+stack_t stack_create_by(int);
+void    stack_push_ptr(stack_t*, void*);
+void*   stack_top_ptr(stack_t*);
+void*   stack_pop_ptr(stack_t*);
+int     stack_size(stack_t*);
+void    stack_clear(stack_t*);
+void    stack_free(stack_t*);
+int     stack_is_empty(stack_t*);
 #define stack_create(type) ({stack_create_by(sizeof(type));})
 #define stack_push(s, value) ({typeof(value) _v=(value); stack_push_ptr((s), &_v);})
 #define stack_top(type, s) ({*(type*)stack_top_ptr((s));})
@@ -40,17 +40,17 @@ typedef struct{
     int _rear;
     int _data_size;
     int _max_size;
-} queue;
+} queue_t;
 
-queue queue_create_by(int, int);
-void  queue_push_ptr(queue*, void*);
-void* queue_front_ptr(queue*);
-void* queue_back_ptr(queue*);
-void* queue_pop_front_ptr(queue*);
-void* queue_pop_back_ptr(queue*);
-int   queue_size(queue*);
-void  queue_clear(queue*);
-void  queue_free(queue*);
+queue_t queue_create_by(int, int);
+void    queue_push_ptr(queue_t*, void*);
+void*   queue_front_ptr(queue_t*);
+void*   queue_back_ptr(queue_t*);
+void*   queue_pop_front_ptr(queue_t*);
+void*   queue_pop_back_ptr(queue_t*);
+int     queue_size(queue_t*);
+void    queue_clear(queue_t*);
+void    queue_free(queue_t*);
 #define queue_create(type, size) ({queue_create_by(sizeof(type), size);})
 #define queue_push(q, value) ({typeof(value) _v=(value); queue_push_ptr((q), &_v);})
 #define queue_front(type, q) ({*(type*)queue_front_ptr((q));})
